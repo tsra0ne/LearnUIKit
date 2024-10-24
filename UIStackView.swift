15546de7cd76e6cc7c27064a57f2afab6ce7fe58
Sravan <tsravangoud164@gmail.com>
2024-10-24 13:09:38 +0530

    Update UIStackView.swift

--- UIStackView.swift
+++ UIStackView.swift
@@ -30,8 +30,6 @@ class ViewController: UIViewController {
     func configureColorViews() {
         for color in colors {
             let colorView = UIView()
-            colorView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-            colorView.widthAnchor.constraint(equalToConstant: 100).isActive = true
             colorView.backgroundColor = color
             colorView.layer.cornerRadius = 10
             stack.addArrangedSubview(colorView)
@@ -39,19 +37,22 @@ class ViewController: UIViewController {
     }
     
     func configureStacks() {
-        stack.axis = .horizontal
-        stack.spacing = 5
-        stack.distribution = .equalSpacing
-        stack.alignment = .center
+        stack.axis = .vertical
+        stack.spacing = 25
+        stack.distribution = .fillEqually
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.directionalLayoutMargins = .init(top: 40, leading: 40, bottom: 40, trailing: 40)
+//        stack.alignment = .center
         stack.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func configureConstraints() {
+        let margins = view.layoutMarginsGuide
         NSLayoutConstraint.activate([
-            stack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            stack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stack.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            stack.topAnchor.constraint(equalTo: margins.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            stack.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
         ])
     }
 }
