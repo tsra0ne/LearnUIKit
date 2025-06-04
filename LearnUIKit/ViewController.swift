4c1e5a0c4594f753631f37b4fbc13bca8f1bb20f
Sravan Goud <tsravangoud164@gmail.com>
2025-06-04 09:31:02 +0530

    added uilabel

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    var titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupUI()
+        setupConstraints()
     }
 
+    func setupUI() {
+        view.backgroundColor = .white
+        
+        titleLabel.text = "This is a very long text that needs to fit"
+        titleLabel.font = .systemFont(ofSize: 20)
+        titleLabel.adjustsFontSizeToFitWidth = true
+        titleLabel.minimumScaleFactor = 0.5
+        titleLabel.numberOfLines = 1
+        titleLabel.lineBreakMode = .byTruncatingTail
+        titleLabel.textAlignment = .center
+        titleLabel.shadowColor = .gray
+        titleLabel.shadowOffset = CGSize(width: 2, height: 2)
+        titleLabel.layer.shadowOpacity = 0.5
+        titleLabel.layer.shadowRadius = 4.0
+        titleLabel.layer.masksToBounds = false
+        view.addSubview(titleLabel)
+    }
+    
+    func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
 }
 
