397f5f99f830b64eaea1d6798320fb2a4821dc68
Sravan Goud <tsravangoud164@gmail.com>
2025-06-05 13:56:33 +0530

    added uiswitch

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let switchControl = UISwitch()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
     }
 
+    func setupUI() {
+        switchControl.isOn = false
+        switchControl.onTintColor = .systemGreen
+        switchControl.thumbTintColor = .white
+        switchControl.isEnabled = true
+        switchControl.addTarget(self, action: #selector(switchPressed), for: .valueChanged)
+        
+        view.addSubview(switchControl)
+        
+        switchControl.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func switchPressed(_ sender: UISwitch) {
+        NSLog("Switch is \(sender.isOn ? "on" : "off")")
+    }
 
 }
 
