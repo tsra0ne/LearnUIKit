b0001c10738746816a67023ed0b91baf359cf3ec
Sravan Goud <tsravangoud164@gmail.com>
2025-06-05 14:28:39 +0530

    added uistackview

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
     }
 
+    func setupUI() {
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        
+        view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        for color in [UIColor.red, UIColor.green, UIColor.blue] {
+            let view = UIView()
+            view.backgroundColor = color
+            view.snp.makeConstraints { make in
+                make.width.height.equalTo(100)
+            }
+            stackView.addArrangedSubview(view)
+        }
+    }
 
 }
 
