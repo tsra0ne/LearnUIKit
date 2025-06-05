79ffe2f60f2c20edf80ac7ff0b8249cb3e805b84
Sravan Goud <tsravangoud164@gmail.com>
2025-06-05 14:07:34 +0530

    added uiprogressview

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let progressView = UIProgressView(progressViewStyle: .default)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        simulateProgress()
     }
 
+    func setupUI() {
+        progressView.progress = 0
+        progressView.progressTintColor = .systemBlue
+        progressView.trackTintColor = .systemGray2
+        
+        view.addSubview(progressView)
+        
+        progressView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    func simulateProgress() {
+        var currentProgress: Float = 0.0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            currentProgress += 0.05
+            self.progressView.setProgress(currentProgress, animated: true)
+            if currentProgress >= 1.0 {
+                timer.invalidate()
+            }
+        }
+    }
 
 }
 
