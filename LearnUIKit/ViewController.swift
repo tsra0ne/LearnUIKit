d88169f538df4b8ce0d72904ad96790291c63da4
Sravan Goud <tsravangoud164@gmail.com>
2025-06-05 15:38:25 +0530

    added uiimageview

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
     }
 
+    func setupUI() {
+        imageView.image = UIImage(named: "photo")
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        imageView.addGestureRecognizer(tapGesture)
+        
+        view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.width.equalTo(200)
+        }
+    }
+    
+    @objc func imageTapped() {
+        NSLog("Image tapped!")
+    }
 
 }
 
