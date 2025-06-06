43cb2f08c0c2b7d9fff2ee4b6b89d9eecf123890
Sravan Goud <tsravangoud164@gmail.com>
2025-06-06 07:34:24 +0530

    added uipickerview

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let textField = UITextField()
+    let pickerView = UIPickerView()
+    let data = ["India", "US", "Canada", "Germany", "France"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
     }
-
+    
+    func setupUI() {
+        textField.placeholder = "Select an option"
+        textField.borderStyle = .roundedRect
+        textField.inputView = pickerView
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissPicker))
+        toolbar.setItems([UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), doneButton], animated: false)
+        textField.inputAccessoryView = toolbar
+        
+        view.addSubviews(textField, pickerView)
+        
+        textField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        pickerView.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        textField.text = data[0]
+        pickerView.selectRow(0, inComponent: 0, animated: false)
+    }
+    
+    @objc func dismissPicker() {
+        textField.resignFirstResponder()
+    }
 
 }
 
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textField.text = data[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = (view as? UILabel) ?? UILabel()
+        label.text = data[row]
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .systemBlue
+        return label
+    }
+}
