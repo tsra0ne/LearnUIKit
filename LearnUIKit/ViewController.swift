eb1d0fb41ceac7149a5767305db3c125134055ae
Sravan Goud <tsravangoud164@gmail.com>
2025-06-06 07:08:51 +0530

    added uidatepicker

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let datePicker = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupUI()
     }
 
+    func setupUI() {
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.minimumDate = Date()
+        datePicker.maximumDate = Calendar.current.date(byAdding: .year, value: 1, to: Date())
+        datePicker.tintColor = .systemBlue
+        datePicker.backgroundColor = .systemGray6
+        datePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+        
+        view.addSubview(datePicker)
+        
+        datePicker.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func dateChanged(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        NSLog("Selected date: \(formatter.string(from: sender.date))")
+    }
 
 }
 
