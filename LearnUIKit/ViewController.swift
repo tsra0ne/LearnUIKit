f9783a10161886a10b20d161522acb86e0f5e08a
Sravan Goud <tsravangoud164@gmail.com>
2025-06-11 16:53:24 +0530

    added alerts

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        button.setTitle("Show Alert", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Alert", message: "This is a alert", preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Name"
+            textField.autocorrectionType = .no
+        }
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { _ in
+            guard let textField = alert.textFields?.first, let value = textField.text else { return }
+            NSLog(value)
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(submitAction)
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
 
 }
 
