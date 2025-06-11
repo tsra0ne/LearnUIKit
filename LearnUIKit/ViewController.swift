93a56942e786ce14c8a307fd67d3406161228142
Sravan Goud <tsravangoud164@gmail.com>
2025-06-11 12:03:44 +0530

    added context menus

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let newView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        let interaction = UIContextMenuInteraction(delegate: self)
+        newView.addInteraction(interaction)
+        newView.backgroundColor = .systemPurple
+        view.addSubview(newView)
+        newView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+    }
 }
 
+extension ViewController: UIContextMenuInteractionDelegate {
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+            let copyAction = UIAction(title: "Copy", image: UIImage(systemName: "doc.on.doc")) { _ in
+                print("Copy action triggered")
+            }
+            let shareAction = UIAction(title: "Share", image: UIImage(systemName: "square.and.arrow.up")) { _ in
+                print("Share action triggered")
+            }
+            return UIMenu(title: "", children: [copyAction, shareAction])
+        }
+    }
+}
