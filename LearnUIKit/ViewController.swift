3d6a02220989b366bb8832e4334d0974edbed175
Sravan Goud <tsravangoud164@gmail.com>
2025-06-11 12:55:00 +0530

    added action sheets

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        button.setTitle("Show Action Sheet", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func showActionSheet(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Options", message: "Choose an action", preferredStyle: .actionSheet)
+        
+        let action1 = UIAlertAction(title: "Edit", style: .default)
+        let action2 = UIAlertAction(title: "Delete", style: .destructive)
+        let action3 = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        alertController.addAction(action3)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
