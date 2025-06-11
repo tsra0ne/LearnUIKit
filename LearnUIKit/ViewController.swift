a0efec2d442d2a30898519499437b6ef4ae22641
Sravan Goud <tsravangoud164@gmail.com>
2025-06-11 11:33:45 +0530

    added uiactivityviewcontroller

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        button.setTitle("Share", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(shareTapped), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func shareTapped(_ sender: UIButton) {
+        let url = URL(string: "https://swiftrivals.com/blog/integrating-swiftui-into-uikit")!
+        let activityViewController = UIActivityViewController(activityItems: [ url ], applicationActivities: nil)
+        present(activityViewController, animated: true)
+    }
 
 }
 
