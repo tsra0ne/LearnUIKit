7619599dd0560bc130a346f895b31024f6023013
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 23:09:53 +0530

    added uistepper

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let stepper = UIStepper()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        stepper.value = 0
+        stepper.minimumValue = 0
+        stepper.maximumValue = 100
+        stepper.stepValue = 10
+        stepper.autorepeat = true
+        stepper.wraps = true
+        stepper.addTarget(self, action: #selector(valueChanged), for: .valueChanged)
+        
+        view.addSubview(stepper)
+        stepper.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func valueChanged(_ sender: UIStepper) {
+        NSLog("Value: \(sender.value)")
+    }
 
 }
 
