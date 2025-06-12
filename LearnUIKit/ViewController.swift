8180e858dd40043b8fced4d1e149333da7d7c2fa
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 14:32:23 +0530

    added sheets

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        button.setTitle("Show Sheet", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
+    @objc func buttonTapped(_ sender: UIButton) {
+        let sheetViewController = SheetViewController()
+        sheetViewController.modalPresentationStyle = .pageSheet
+        
+        if let sheet = sheetViewController.sheetPresentationController {
+            let customDetent = UISheetPresentationController.Detent.custom(identifier: .init("custom")) { context in
+                return context.maximumDetentValue * 0.3
+            }
+            sheet.detents = [customDetent, .medium(), .large()]
+            sheet.prefersGrabberVisible = true
+            sheet.prefersEdgeAttachedInCompactHeight = true // Attach to bottom in compact height
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false // Prevent scrolling from expanding detent
+            sheet.largestUndimmedDetentIdentifier = .medium
+            sheet.selectedDetentIdentifier = .medium
+            sheet.preferredCornerRadius = 20 // Round corners
+            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true // Respect preferredContentSize
+        }
+        
+        present(sheetViewController, animated: true)
+    }
 
 }
 
+class SheetViewController: UIViewController {
+    
+    let button = UIButton()
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemRed
+        super.viewDidLoad()
+        button.setTitle("Dismiss Sheet", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
