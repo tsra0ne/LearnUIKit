ccfb0000660759d9cd66ad75d007dffeabf43d55
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 20:37:37 +0530

    added phpickerviewcontroller

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import SnapKit
+import PhotosUI
 
 class ViewController: UIViewController {
+    
+    let imageView = UIImageView()
+    let button = UIButton()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        imageView.backgroundColor = .systemPurple
+        imageView.contentMode = .scaleAspectFit
+        
+        button.setTitle("Set Image", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        view.addSubviews(imageView, button)
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(150)
+        }
+        
+        button.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 
 }
+
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        guard let result = results.first else { return }
+        if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
+            result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                DispatchQueue.main.async {
+                    if let image = image as? UIImage {
+                        self?.imageView.image = image
+                    } else if let error = error {
+                        NSLog("Error loading image: \(error)")
+                    }
+                }
+            }
+        }
+    }
+}
+
 
