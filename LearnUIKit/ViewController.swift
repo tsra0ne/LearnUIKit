c419e3148447434ee09aeaa1ab92f04af87bb6aa
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 13:33:37 +0530

    added userdefaults

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let segmentControl = UISegmentedControl(items: ["Option 1", "Option 2", "Option 3"])
+    let selectedSegmentIndex = UserDefaults.standard.integer(forKey: "com.swiftrivals.LearnUIKit.selectedSegmentIndex")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        segmentControl.selectedSegmentIndex = selectedSegmentIndex
+        segmentControl.addTarget(self, action: #selector(valueChanged), for: .valueChanged)
+        view.addSubview(segmentControl)
+        segmentControl.snp.makeConstraints{ make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func valueChanged(_ sender: UISegmentedControl) {
+        UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: "com.swiftrivals.LearnUIKit.selectedSegmentIndex")
+    }
 
 }
 
