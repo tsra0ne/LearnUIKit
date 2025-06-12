76bb2bb31558af09e3a84e61f2d80d5987611ec5
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 15:26:16 +0530

    added uiimagepickercontroller

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let imageView = UIImageView()
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        imageView.backgroundColor = .systemPurple
+        imageView.contentMode = .scaleAspectFit
+        
+        button.setTitle("Set Image", for: .normal)
+        button.configuration = .filled()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        view.addSubviews(imageView, button)
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(150)
+        }
+        
+        button.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.mediaTypes = ["public.image"]
+        
+        present(picker, animated: true)
+    }
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            imageView.image = editedImage
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            imageView.image = originalImage
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+}
