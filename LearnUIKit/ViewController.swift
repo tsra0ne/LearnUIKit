daeccb93229404d07fbcf34b8f48cf3f9bd93cee
Sravan Goud <tsravangoud164@gmail.com>
2025-06-12 22:48:57 +0530

    added uicolorwell

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let colorWell = UIColorWell()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
 
+    func setupUI() {
+        colorWell.selectedColor = .red
+        colorWell.supportsAlpha = true
+        colorWell.title = "Select Background Color"
+        colorWell.addTarget(self, action: #selector(colorChanged), for: .valueChanged)
+        
+        view.addSubview(colorWell)
+        colorWell.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func colorChanged(_ sender: UIColorWell) {
+        view.backgroundColor = sender.selectedColor
+    }
 
 }
 
