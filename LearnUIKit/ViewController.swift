6e69ef0a9ffb1cd4ad9405b869b413a8c939d07b
Sravan Goud <tsravangoud164@gmail.com>
2025-06-14 11:36:01 +0530

    added uipageviewcontroller and uipagecontrol

--- LearnUIKit/ViewController.swift
+++ LearnUIKit/ViewController.swift
@@ -6,14 +6,88 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let pageViewController = UIPageViewController(
+        transitionStyle: .scroll,
+        navigationOrientation: .horizontal,
+        options: nil
+    )
+    
+    let pageControl = UIPageControl()
+    
+    var pages: [UIViewController] = []
+    var currentIndex: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupUI()
     }
-
+    
+    func setupUI() {
+        
+        let page1 = UIViewController()
+        page1.view.backgroundColor = .red
+        
+        let page2 = UIViewController()
+        page2.view.backgroundColor = .purple
+        
+        let page3 = UIViewController()
+        page3.view.backgroundColor = .cyan
+        
+        pages = [page1, page2, page3]
+        
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        
+        pageViewController.setViewControllers([pages[0]], direction: .forward, animated: true)
+        
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = 0
+        pageControl.backgroundStyle = .prominent
+        pageControl.isUserInteractionEnabled = false
+        
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParent: self)
+        
+        view.addSubview(pageControl)
+        
+        pageViewController.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+        }
+    }
 
 }
 
+extension ViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController), currentIndex > 0 else { return nil }
+        return pages[currentIndex - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController), currentIndex < pages.count - 1 else { return nil }
+        return pages[currentIndex + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            if let index = currentIndex {
+                pageControl.currentPage = index
+            }
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        currentIndex = pages.firstIndex(of: pendingViewControllers.first!)
+    }
+}
